a66c639d423844779f2bba0b8738e3f1aa27667f
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-07-03 14:07:58 -0500

    going to branch for the contact section

--- src/Home/index.js
+++ src/Home/index.js
@@ -8,17 +8,17 @@ class Home extends Component{
 	}
 
 
+				// <img id="homeImg" alt="wedding-planning" src="https://www.hellomagazine.com/imagenes/brides/2019011066459/wedding-planning-apps-for-brides/0-313-188/wedding-planning-t.jpg" />
 
   render(){
 	return (
-		<div id="homePage">
+		<div>
 			<div className="form">
 				<h2 className="animated 1s flipInX">Welcome To Wedding Exchange!</h2>
 				<h4>
 					Wedding Exchange is the premier online platform for connecting individuals that want to 
 					buy, sell, and trade wedding goods.
 				</h4>
-				<img id="homeImg" alt="wedding-planning" src="https://www.hellomagazine.com/imagenes/brides/2019011066459/wedding-planning-apps-for-brides/0-313-188/wedding-planning-t.jpg" />
 				<div>
 					<Link to="/register"><button>Register New Account</button></Link><br /><br />
 					<Link to="/login"><button>Login</button></Link>
--- src/Login/index.js
+++ src/Login/index.js
@@ -68,6 +68,7 @@ class Login extends React.Component {
 
 
   render(){
+      console.log(this.state);
       return (
         <div className="loginContainer">
           <form className="form" onSubmit={this.handleLogin}>
