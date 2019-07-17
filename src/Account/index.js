329d869195a85ccfa8eabd7a11d7d52860f12cf3
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-07-17 16:51:09 -0500

    touching up

--- src/Account/index.js
+++ src/Account/index.js
@@ -238,7 +238,6 @@ class Account extends React.Component {
 												<button onClick={this.handleLogout}>Logout</button>
 												<button onClick={this.handleEditAccount}>Edit Account</button>
 												<button onClick={this.handleDeleteAccount}>Delete Account</button>
-
 											</div>
 
 										</div>
